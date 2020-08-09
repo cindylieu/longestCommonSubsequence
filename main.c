f38731313cc20d4537211644e949e5d2e1b8e04f
Cindy Lieu <lieuzcindy@gmail.com>
2020-08-09 18:27:24 -0400

    print longest common subsequence

--- main.c
+++ main.c
@@ -3,57 +3,70 @@
 #include <string.h>
 
 int longestCommonSubsequence(char * text1, char * text2);
-int lcs(char *text1, char *text2, int len1, int len2);
+void lcs(char *text1, char *text2);
 int max (int a, int b);
 
 int main() {
-    printf("%d", longestCommonSubsequence("abcde", "ace"));
+
+    printf("%d\n", longestCommonSubsequence("abcde", "ace"));
+
+    printf("%d\n", longestCommonSubsequence("abc", "ace"));
+
+    printf("%d\n", longestCommonSubsequence("dgf", "ace"));
+
+    lcs("abcde", "ace");
+
     return 0;
 }
 
 int longestCommonSubsequence(char * text1, char * text2){
 
-    if (text1 == NULL || text2 == NULL || strlen(text1) == 0 || strlen(text2) == 0) {
+    int len1 = strlen(text1);
+    int len2 = strlen(text2);
+
+    if (text1 == NULL || text2 == NULL || len1 == 0 || len2 == 0) {
         return 0;
     }
 
-    return lcs(text1, text2, strlen(text1), strlen(text2));
-}
-
-int lcs(char *text1, char *text2, int len1, int len2) {
-
-    int row = len1+1;
-    int col = len2+1;
-
-    int dp[row][col];
-
-    for (int r = 0; r < len1; r++) {
-        for (int c = 0; c < len2; c++) {
-            dp[r][c] = 0;
-        }
-    }
-
+    int dp[len1+1][len2+1];
     int i, j;
 
-    for (i = 0; i < len1; i++) {
-        for (j = 0; j < len2; j++ ) {
-            if (text1[i-1] == text2[j-1]) {
-                dp[i][j] = dp[i-1][j-1] + 1;
+    for (i = 0; i < len1+1; i++) {
+        for (j = 0; j < len2+1; j++ ) {
+            if (i == 0||j == 0) {
+                dp[i][j] = 0;
+            } else if (text1[i-1] != text2[j-1]) {
+                if (dp[i-1][j] >= dp[i][j-1]) {
+                    dp[i][j] = dp[i-1][j];
+                } else
+                    dp[i][j] = dp[i][j-1];
             } else {
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                dp[i][j] = dp[i-1][j-1] + 1;
             }
         }
     }
-
     return dp[len1][len2];
 }
 
-int max (int a, int b) {
-    if (a == b) {
-        return a;
-    } else if (a > b) {
-        return a;
-    } else {
-        return b;
+void lcs(char * text1, char * text2){
+
+    int len1 = strlen(text1);
+    int len2 = strlen(text2);
+
+    int dp[len1+1][len2+1];
+    int i, j;
+
+    for (i = 0; i < len1+1; i++) {
+        for (j = 0; j < len2+1; j++ ) {
+            if (i == 0||j == 0) {
+                dp[i][j] = 0;
+            } else if (text1[i-1] != text2[j-1]) {
+                if (dp[i-1][j] >= dp[i][j-1]) {
+                } else
+                    printf("%c",text1[i-1]);
+            } else {
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+        }
     }
 }
